4ef3fdee965a79417fae9d6c06659d723d20f0b0
Rubén Chiquin <randreu28@gmail.com>
2023-09-20 17:12:01 +0200

    Completes ex.3.3-6

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import type { Request, Response } from "express";
+import z from "zod";
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 const persons = [
@@ -36,6 +38,51 @@ app.get("/info", (_req: Request, res: Response) => {
   <p>Phone book has info for ${persons.length}<p/>
   <p>${new Date().toString()}</p>
   `);
+});
+
+app.get("/api/persons/:id", (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+
+  res.send(persons.find((person) => person.id === id));
+});
+
+app.delete("/api/persons/:id", (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+
+  res.send(persons.filter((person) => person.id !== id));
+});
+
+app.post("/api/persons", (req: Request, res: Response) => {
+  const data = req.body;
+
+  console.log(data);
+
+  const personSchema = z.object({
+    name: z.string(),
+    number: z.string(),
+  });
+
+  const { success } = personSchema.safeParse(data);
+
+  if (!success) {
+    res.status(400).json({ error: "invalid body" }).end();
+    return;
+  }
+
+  const parsedData = data as z.infer<typeof personSchema>;
+
+  const isNameRegistered = persons.some(
+    (person) => person.name === parsedData.name
+  );
+
+  if (isNameRegistered) {
+    res.status(400).json({ error: "Name must be unique" }).end();
+    return;
+  }
+
+  persons.push({ ...parsedData, id: Math.floor(Math.random() * 999999) });
+
+  res.send(persons);
 });
 
 app.listen(port, () => {
