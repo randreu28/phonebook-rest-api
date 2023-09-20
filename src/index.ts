f037d6d433983c239754069391f30e80ab33265b
Rubén Chiquin <randreu28@gmail.com>
2023-09-20 16:29:47 +0200

    Completes ex.3.1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
-import express from "express" 
+import express from "express"
 import type { Request, Response } from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world!');
+const persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (req: Request, res: Response) => {
+  res.send(persons);
 });
 
 app.listen(port, () => {
