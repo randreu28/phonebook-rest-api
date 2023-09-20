4717053cd55dd84ae2c93a986a1eb748c410d50f
Rubén Chiquin <randreu28@gmail.com>
2023-09-20 16:34:56 +0200

    Completes ex.3.2

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,41 @@
-import express from "express"
-import type { Request, Response } from 'express';
+import express from "express";
+import type { Request, Response } from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const persons = [
   {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
   {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/persons', (req: Request, res: Response) => {
+app.get("/api/persons", (_req: Request, res: Response) => {
   res.send(persons);
+});
+
+app.get("/info", (_req: Request, res: Response) => {
+  res.send(`
+  <p>Phone book has info for ${persons.length}<p/>
+  <p>${new Date().toString()}</p>
+  `);
 });
 
 app.listen(port, () => {
